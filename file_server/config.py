736a4bdc92067db33e038ea5e4d1ba2be01d1741
Karol Magier <karol.magier@gmail.com>
2020-04-10 13:15:40 +0200

    Small change

--- file_server/config.py
+++ file_server/config.py
@@ -2,10 +2,10 @@ import os
 
 DB_TYPE = 'postgresql'
 DB_CONNECTOR = 'psycopg2'
-USERNAME = 'karol'
-PASSWORD = '123456'
+USERNAME = os.environ.get('POSTGRES_USER')
+PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 PORT = '5432'
 HOST = 'postgresdb' + ':' + PORT
-DB_NAME = 'database'
+DB_NAME = os.environ.get('POSTGRES_DB')
 
 DB_URI = DB_TYPE + "+" + DB_CONNECTOR +'://' + USERNAME +':' + PASSWORD + "@" + HOST + '/' + DB_NAME
